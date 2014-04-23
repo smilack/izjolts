b44087022e33df3c4812d470e42ca446ce70c1fc
Tom Smilack <tom@smilack.net>
2014-04-23 11:45:03 -0500

    Refactor move-piece, start on can-move

--- desktop/src-common/izjolts/pieces.clj
+++ desktop/src-common/izjolts/pieces.clj
@@ -1,14 +1,37 @@
 (ns izjolts.pieces
-  (:require [izjolts.utils :as u]))
+  (:require [izjolts.utils :as u]
+            [izjolts.data :as d]))
 
 (def piece-names [:I :Z :J :O :L :T :S])
+
+(defn can-move
+  [piece dx dy]
+  (every?
+    #(and
+       (< 0 (+ (:bucket-x %) dx) u/bucket-width)
+       (< 0 (+ (:bucket-y %) dy) u/bucket-height)
+       (comment "check for existing monominoes"))
+    (:entities piece)))
+
+(defn move-monomino
+  [monomino dx dy]
+  (let [bucket-x (+ (:bucket-x monomino dx))
+        bucket-y (+ (:bucket-y monomino) dy)
+        [x y] (u/bucket->screen bucket-x bucket-y)]
+    (assoc monomino :x x :y y :bucket-x bucket-x :bucket-y bucket-y)))
+
+(defn move-piece
+  [piece dx dy]
+  (let [new-entities (map #(move-monomino % dx dy) (:entities piece))]
+    (assoc piece :entities new-entities)))
 
 (defn piece
   "Creates a new piece in the starting area with default rotation."
   [name]
-  nil)
+  (update-position
+    ))
 
-(defn move-piece
+(defn shift-piece
   [piece direction]
   (let [dx (case direction
              :down 0
@@ -19,12 +42,4 @@
              :left 0
              :right 0)]
     (if (can-move? piece dx dy)
-      (let [bucket-x (+ (:bucket-x piece dx))
-            bucket-y (+ (:bucket-y piece) dy)
-            [x y] (u/bucket->screen bucket-x bucket-y)]
-        (assoc piece :x x :y y :bucket-x bucket-x :bucket-y bucket-y)))))
-
-(defn can-move
-  [piece dx dy]
-  true)
-  
+      (move-piece piece dx dy))))
