ad1475bc09fdb27c0182bad70a1081fc5c2e8609
Tom Smilack <tom@smilack.net>
2014-04-23 10:26:54 -0500

    Add piece data

---
+++ desktop/src-common/izjolts/data.clj
@@ -0,0 +1,96 @@
+(ns izjolts.data)
+
+(def pieces {:I [[[false true  false false]
+                  [false true  false false]
+                  [false true  false false]
+                  [false true  false false]]
+                 
+                 [[false false false false]
+                  [true  true  true  true ]
+                  [false false false false]
+                  [false false false false]]]
+             
+             :Z [[[false false false false]
+                  [true  true  false false]
+                  [false true  true  false]
+                  [false false false false]]
+                 
+                 [[false false true  false]
+                  [false true  true  false]
+                  [false true  false false]
+                  [false false false false]]]
+             
+             :J [[[false true  false false]
+                  [false true  false false]
+                  [true  true  false false]
+                  [false false false false]]
+                 
+                 [[true  false false false]
+                  [true  true  true  false]
+                  [false false false false]
+                  [false false false false]]
+                 
+                 [[false true  true  false]
+                  [false true  false false]
+                  [false true  false false]
+                  [false false false false]]
+                 
+                 [[false false false false]
+                  [true  true  true  false]
+                  [false false true  false]
+                  [false false false false]]]
+             
+             :O [[[false false false false]
+                  [false true  true  false]
+                  [false true  true  false]
+                  [false false false false]]]
+             
+             :L [[[false true  false false]
+                  [false true  false false]
+                  [false true  true  false]
+                  [false false false false]]
+                 
+                 [[false false false false]
+                  [true  true  true  false]
+                  [true  false false false]
+                  [false false false false]]
+                 
+                 [[true  true  false false]
+                  [false true  false false]
+                  [false true  false false]
+                  [false false false false]]
+                 
+                 [[false false true  false]
+                  [true  true  true  false]
+                  [false false false false]
+                  [false false false false]]]
+             
+             :T [[[false false false false]
+                  [true  true  true  false]
+                  [false true  false false]
+                  [false false false false]]
+                 
+                 [[false true  false false]
+                  [true  true  false false]
+                  [false true  false false]
+                  [false false false false]]
+                 
+                 [[false true  false false]
+                  [true  true  true  false]
+                  [false false false false]
+                  [false false false false]]
+                 
+                 [[false true  false false]
+                  [false true  true  false]
+                  [false true  false false]
+                  [false false false false]]]
+             
+             :S [[[false false false false]
+                  [false true  true  false]
+                  [true  true  false false]
+                  [false false false false]]
+                 
+                 [[false true  false false]
+                  [false true  true  false]
+                  [false false true  false]
+                  [false false false false]]]})
