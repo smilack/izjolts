e39bfef62b233ad6702c1b3fce5e22b4ac6c0f4d
Tom Smilack <tom@smilack.net>
2014-04-23 10:56:49 -0500

    Add move-piece function

--- desktop/src-common/izjolts/pieces.clj
+++ desktop/src-common/izjolts/pieces.clj
@@ -1,3 +1,30 @@
-(ns izjolts.pieces)
+(ns izjolts.pieces
+  (:require [izjolts.utils :as u]))
 
 (def piece-names [:I :Z :J :O :L :T :S])
+
+(defn piece
+  "Creates a new piece in the starting area with default rotation."
+  [name]
+  nil)
+
+(defn move-piece
+  [piece direction]
+  (let [dx (case direction
+             :down 0
+             :left -1
+             :right 1)
+        dy (case direction
+             :down -1
+             :left 0
+             :right 0)]
+    (if (can-move? piece dx dy)
+      (let [bucket-x (+ (:bucket-x piece dx))
+            bucket-y (+ (:bucket-y piece) dy)
+            [x y] (u/bucket->screen bucket-x bucket-y)]
+        (assoc piece :x x :y y :bucket-x bucket-x :bucket-y bucket-y)))))
+
+(defn can-move
+  [piece dx dy]
+  true)
+  
