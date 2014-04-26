a2ca0d8461c0f809056ac1ba77d29103c321708c
Tom Smilack <tom@smilack.net>
2014-04-25 23:49:44 -0500

    Remove completed lines

--- desktop/src-common/izjolts/core.clj
+++ desktop/src-common/izjolts/core.clj
@@ -23,6 +23,37 @@
         moved (p/shift-piece entities current direction)]
     moved))
 
+(defn find-complete-lines
+  [entities]
+  (for [r (range u/bucket-height)
+        :when (= u/bucket-width
+                 (count (filterv #(= r (:bucket-y % -1)) entities)))]
+    r))
+
+(defn filter-lines
+  [entities complete-lines]
+  (filterv
+    (fn [monomino]
+      (not-any?
+        (fn [line] (= line (:bucket-y monomino)))
+        complete-lines))
+    entities))
+
+(defn remove-complete-lines
+  [entities]
+  (let [complete-lines (find-complete-lines entities)
+        lines-removed (filter-lines entities complete-lines)
+        shift-list (for [n (range 20)] (count (filter (partial > n ) complete-lines)))
+        shift-map (zipmap (range 20) shift-list)]
+    (map
+      (fn [entity]
+        (if (and (contains? entity :bucket-y) (:monomino? entity))
+          (let [y (:bucket-y entity)
+                dy (get shift-map y)]
+            (p/move-monomino entity 0 (- dy)))
+          entity))
+      lines-removed)))
+
 (defscreen main-screen
   :on-show
   (fn on-show[screen entities]
@@ -60,8 +91,10 @@
       (next-piece entities blocks)
       (if-let [moved (move-current entities :down)]
         (replace {(u/find-e :current? entities) moved} entities)
-        (let [current (u/find-e :current? entities)]
-          (concat (filterv (complement :current?) entities) (:entities current)))))))
+        (let [current (u/find-e :current? entities)
+              monominoes (:entities current)
+              e-with-m (concat (filterv (complement :current?) entities) monominoes)]
+          (remove-complete-lines e-with-m))))))
 
 (defgame izjolts
   :on-create
